506505c581827caffd3a09fab988efbe89aa70df
Anton Kovalyov <anton@kovalyov.net>
2010-12-16 13:31:28 -0800

    Basic styles for the admin interface

--- gargoyle/views.py
+++ gargoyle/views.py
@@ -1,15 +1,5 @@
 from django.conf import settings
-from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 
-def render(template, user_values=None):
-    values = {
-        "media": reverse("gargoyle-media"),
-    }
-    print reverse("gargoyle-media")
-    if user_values:
-        values.updated(user_values)
-    return render_to_response(template, values)
-
 def index(request):
-    return render("gargoyle/index.html")
+    return render_to_response("gargoyle/index.html")
