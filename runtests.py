120ce62bca2b4476acbaf4e88149eb15313744d9
Anton Kovalyov <anton@kovalyov.net>
2010-12-16 10:40:01 -0800

    Added generic package files

---
+++ runtests.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import logging
+import sys
+from os.path import dirname, abspath
+
+logging.getLogger('gargoyle').addHandler(logging.StreamHandler())
+
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure(
+        DATABASE_ENGINE='sqlite3',
+        # HACK: this fixes our threaded runserver remote tests
+        # DATABASE_NAME='test_sentry',
+        # TEST_DATABASE_NAME='test_sentry',
+        INSTALLED_APPS=[
+            'django.contrib.auth',
+            'django.contrib.admin',
+            'django.contrib.sessions',
+            'django.contrib.sites',
+
+            # Included to fix Disqus' test Django which solves IntegrityMessage case
+            'django.contrib.contenttypes',
+        ],
+        ROOT_URLCONF='',
+        DEBUG=False,
+        SITE_ID=1,
+        BROKER_HOST="localhost",
+        BROKER_PORT=5672,
+        BROKER_USER="guest",
+        BROKER_PASSWORD="guest",
+        BROKER_VHOST="/",
+        CELERY_ALWAYS_EAGER=True,
+        SENTRY_THRASHING_LIMIT=0,
+        TEMPLATE_DEBUG=True,
+    )
+
+from django.test.simple import run_tests
+
+def runtests(*test_args):
+    if not test_args:
+        test_args = ['gargoyle']
+    parent = dirname(abspath(__file__))
+    sys.path.insert(0, parent)
+    failures = run_tests(test_args, verbosity=1, interactive=True)
+    sys.exit(failures)
+
+if __name__ == '__main__':
+    runtests(*sys.argv[1:])
---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+
+
+class mytest(test):
+    def run(self, *args, **kwargs):
+        from runtests import runtests
+        runtests()
+        # Upgrade().run(dist=True)
+        # test.run(self, *args, **kwargs)
+
+setup(
+    name='gargoyle',
+    version='0.1.0',
+    author='Disqus',
+    author_email='team@disqus.com',
+    url='http://github.com/disqus/gargoyle',
+    description = 'Switches',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[],
+    test_suite = 'gargoyle.tests',
+    include_package_data=True,
+    cmdclass={"test": mytest},
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
