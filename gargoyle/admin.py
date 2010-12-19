51efdca110623b0969164eef6a198f907ba9b246
David Cramer <dcramer@gmail.com>
2010-12-18 16:00:51 -0800

    basic modeladmin

---
+++ gargoyle/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from gargoyle.models import Switch
+
+class SwitchAdmin(admin.ModelAdmin):
+    list_display = ('label', 'key', 'status')
+    list_filter = ('status',)
+    search_fields = ('label', 'key', 'value')
+
+admin.site.register(SwitchAdmin, Switch)
