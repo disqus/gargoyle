34f8e7f8352ea91ccf56744fada0305719c06495
Szilveszter Farkas <szilveszter.farkas@gmail.com>
2011-02-26 22:53:49 +0100

    Django 1.2.5 compatibility (get CSRF token from cookie).

--- gargoyle/media/js/gargoyle.js
+++ gargoyle/media/js/gargoyle.js
@@ -1,4 +1,30 @@
 $(document).ready(function () {
+    /*
+    CSRF protection snippet copied from
+    http://docs.djangoproject.com/en/dev/ref/contrib/csrf/#ajax
+    */
+    $('html').ajaxSend(function(event, xhr, settings) {
+        function getCookie(name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie != '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = jQuery.trim(cookies[i]);
+                    // Does this cookie string begin with the name we want?
+                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+            // Only send the token to relative URLs i.e. locally.
+            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+        }
+    });
+
     var api = function (url, params, succ) {
         $('#status').show();
         $.ajax({
