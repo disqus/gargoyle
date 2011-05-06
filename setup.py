c3d5836acfe03724d4c57f795297d7013f34eb59
David Cramer <dcramer@gmail.com>
2011-05-05 17:02:55 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class mytest(test):
 
 setup(
     name='gargoyle',
-    version='0.1.3',
+    version='0.1.4',
     author='DISQUS',
     author_email='opensource@disqus.com',
     url='http://github.com/disqus/gargoyle',
