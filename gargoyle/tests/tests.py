17e27cb86d68dfa4ed8fd775b45b193da77f46fb
David Cramer <dcramer@gmail.com>
2011-06-13 18:32:44 -0700

    Added regression tests for constants

--- gargoyle/tests/tests.py
+++ gargoyle/tests/tests.py
@@ -443,6 +443,30 @@ class GargoyleTest(TestCase):
         self.assertTrue(inner_condition[2], '192.168.1.1')
         self.assertFalse(inner_condition[3])
 
+class GargoyleConstantTest(TestCase):
+    def setUp(self):
+        self.gargoyle = SwitchManager(Switch, key='key', value='value', instances=True)
+
+    def test_disabled(self):
+        self.assertTrue(hasattr(self.gargoyle, 'DISABLED'))
+        self.assertEquals(self.gargoyle.DISABLED, 1)
+
+    def test_selective(self):
+        self.assertTrue(hasattr(self.gargoyle, 'SELECTIVE'))
+        self.assertEquals(self.gargoyle.SELECTIVE, 2)
+
+    def test_global(self):
+        self.assertTrue(hasattr(self.gargoyle, 'GLOBAL'))
+        self.assertEquals(self.gargoyle.GLOBAL, 3)
+
+    def test_include(self):
+        self.assertTrue(hasattr(self.gargoyle, 'INCLUDE'))
+        self.assertEquals(self.gargoyle.INCLUDE, 'i')
+
+    def test_exclude(self):
+        self.assertTrue(hasattr(self.gargoyle, 'EXCLUDE'))
+        self.assertEquals(self.gargoyle.EXCLUDE, 'e')
+
 class GargoyleTemplateTagTest(TestCase):
     urls = 'gargoyle.tests.urls'
     
