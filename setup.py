748f5a8e0071df7cf0ce24677b259eb531907b67
David Cramer <dcramer@gmail.com>
2011-12-19 15:59:25 -0800

    Require a newer version of nexus for csrf fixes

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ tests_require = [
 
 setup(
     name='gargoyle',
-    version='0.6.1',
+    version='0.6.2',
     author='DISQUS',
     author_email='opensource@disqus.com',
     url='http://github.com/disqus/gargoyle',
@@ -25,7 +25,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'django-modeldict>=1.1.6',
-        'nexus>=0.2.0',
+        'nexus>=0.2.3',
         'django-jsonfield',
     ],
     license='Apache License 2.0',
