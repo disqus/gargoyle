1eb5ad567c1c12982fdf09432e7054b0f9c586fd
David Cramer <dcramer@gmail.com>
2011-12-28 11:47:58 -0800

    Lock json field at 0.6

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         'django-modeldict>=1.1.6',
         'nexus>=0.2.3',
-        'django-jsonfield',
+        'django-jsonfield==0.6',
     ],
     license='Apache License 2.0',
     tests_require=tests_require,
