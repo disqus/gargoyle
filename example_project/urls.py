9c378e3b87e84d3973519469762e4b27d69c11eb
Jeff Pollard <jeff.pollard@gmail.com>
2012-02-03 14:34:52 -0800

    Remove all the code that is not core to managing switches

--- example_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-import nexus
-
-nexus.autodiscover()
-
-urlpatterns = patterns('',
-    url(r'', include(nexus.site.urls)),
-)
--- gargoyle/__init__.py
+++ gargoyle/__init__.py
@@ -15,28 +15,3 @@ except Exception, e:
     VERSION = 'unknown'
 
 from gargoyle.manager import gargoyle
-
-def autodiscover():
-    """
-    Auto-discover INSTALLED_APPS admin.py modules and fail silently when
-    not present. This forces an import on them to register any admin bits they
-    may want.
-    """
-    import copy
-    from django.conf import settings
-    from django.utils.importlib import import_module
-
-    for app in settings.INSTALLED_APPS:
-        # Attempt to import the app's gargoyle module.
-        before_import_registry = copy.copy(gargoyle._registry)
-        try:
-            import_module('%s.gargoyle' % app)
-        except:
-            # Reset the model registry to the state before the last import as
-            # this import will have to reoccur on the next request and this
-            # could raise NotRegistered and AlreadyRegistered exceptions
-            gargoyle._registry = before_import_registry
-
-    # load builtins
-    __import__('gargoyle.builtins')
-
