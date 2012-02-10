07867356f110026a2249d49fe5e583c42fc2a048
Jeff Pollard <jeff.pollard@gmail.com>
2012-02-10 13:01:24 -0800

    Add another test to make sure you can import settings and change them before importing the singleton

--- tests/test_singleton.py
+++ tests/test_singleton.py
@@ -8,6 +8,8 @@ import gargoyle.models
 
 class TestGargoyle(unittest.TestCase):
 
+    other_engine = dict()
+
     def test_gargoyle_global_is_a_switch_manager(self):
         reload(gargoyle.singleton)
         self.assertIsInstance(gargoyle.singleton.gargoyle,
@@ -18,4 +20,12 @@ class TestGargoyle(unittest.TestCase):
             init.return_value = None
             reload(gargoyle.singleton)
             expected = ((), {'storage': manager.storage_engine})
+            eq_(init.call_args, expected)
+
+    def test_can_change_storage_engine_before_importing(self):
+        with mock.patch('gargoyle.models.Manager') as init:
+            init.return_value = None
+            manager.storage_engine = self.other_engine
+            reload(gargoyle.singleton)
+            expected = ((), dict(storage=self.other_engine))
             eq_(init.call_args, expected)
