3be5c38850f11e4bb909dc1efbd8ffd845a5149d
David Cramer <dcramer@gmail.com>
2012-05-03 11:34:48 -0700

    0.9.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ tests_require = [
 
 setup(
     name='gargoyle',
-    version='0.8.2',
+    version='0.9.0',
     author='DISQUS',
     author_email='opensource@disqus.com',
     url='http://github.com/disqus/gargoyle',
