32704cf218e17fa8269a8ad28873d76afbd2256d
David Cramer <dcramer@gmail.com>
2012-09-13 13:12:53 -0700

    Bump modeldict requirement

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ tests_require = [
 ]
 
 install_requires = [
-    'django-modeldict>=1.3.5',
+    'django-modeldict>=1.3.6',
     'nexus>=0.2.3',
     'django-jsonfield>=0.8.0',
 ]
@@ -23,7 +23,7 @@ install_requires = [
 
 setup(
     name='gargoyle',
-    version='0.10.3',
+    version='0.10.4',
     author='DISQUS',
     author_email='opensource@disqus.com',
     url='http://github.com/disqus/gargoyle',
