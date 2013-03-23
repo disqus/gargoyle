0ccc5a309a5064161e7f6bf85beff36263dfb92a
Michael Nelson <absoludity@gmail.com>
2013-03-23 14:52:51 +0100

    Add two management commands: add_switch and remove_switch.

--- tests/tests.py
+++ tests/tests.py
@@ -8,6 +8,8 @@ import datetime
 from django.conf import settings
 from django.contrib.auth.models import User, AnonymousUser
 from django.core.cache import cache
+from django.core.management.base import CommandError
+from django.core.management import call_command
 from django.http import HttpRequest, Http404, HttpResponse
 from django.test import TestCase
 from django.template import Context, Template, TemplateSyntaxError
@@ -16,6 +18,10 @@ from gargoyle.builtins import IPAddressConditionSet, UserConditionSet, HostCondi
 from gargoyle.decorators import switch_is_active
 from gargoyle.helpers import MockRequest
 from gargoyle.models import Switch, SELECTIVE, DISABLED, GLOBAL, INHERIT
+from gargoyle.management.commands.add_switch import Command as AddSwitchCmd
+from gargoyle.management.commands.remove_switch import (
+    Command as RemoveSwitchCmd
+)
 from gargoyle.manager import SwitchManager
 from gargoyle.testutils import switches
 
@@ -1085,3 +1091,82 @@ class SwitchContextManagerTest(TestCase):
             self.assertFalse(self.gargoyle.is_active('test'))
 
         self.assertEquals(self.gargoyle['test'].status, GLOBAL)
+
+
+class CommandAddSwitchTestCase(TestCase):
+
+    def setUp(self):
+        self.gargoyle = SwitchManager(Switch, key='key', value='value', instances=True, auto_create=True)
+
+    def test_requires_single_arg(self):
+        too_few_too_many = [
+            [],
+            ['one', 'two'],
+        ]
+        for args in too_few_too_many:
+            command = AddSwitchCmd()
+
+            self.assertRaises(CommandError, command.handle, *args)
+
+    def test_add_switch_default_status(self):
+        self.assertNotIn('switch_default', self.gargoyle)
+
+        call_command('add_switch', 'switch_default')
+
+        self.assertIn('switch_default', self.gargoyle)
+        self.assertEqual(GLOBAL, self.gargoyle['switch_default'].status)
+
+    def test_add_switch_with_status(self):
+        self.assertNotIn('switch_disabled', self.gargoyle)
+
+        call_command('add_switch', 'switch_disabled', status=DISABLED)
+
+        self.assertIn('switch_disabled', self.gargoyle)
+        self.assertEqual(DISABLED, self.gargoyle['switch_disabled'].status)
+
+    def test_update_switch_status_disabled(self):
+        Switch.objects.create(key='test', status=GLOBAL)
+        self.assertEqual(GLOBAL, self.gargoyle['test'].status)
+
+        call_command('add_switch', 'test', status=DISABLED)
+
+        self.assertEqual(DISABLED, self.gargoyle['test'].status)
+
+    def test_update_switch_status_to_default(self):
+        Switch.objects.create(key='test', status=DISABLED)
+        self.assertEqual(DISABLED, self.gargoyle['test'].status)
+
+        call_command('add_switch', 'test')
+
+        self.assertEqual(GLOBAL, self.gargoyle['test'].status)
+
+
+class CommandRemoveSwitchTestCase(TestCase):
+
+    def setUp(self):
+        self.gargoyle = SwitchManager(Switch, key='key', value='value', instances=True, auto_create=True)
+
+    def test_requires_single_arg(self):
+        too_few_too_many = [
+            [],
+            ['one', 'two'],
+        ]
+        for args in too_few_too_many:
+            command = RemoveSwitchCmd()
+
+            self.assertRaises(CommandError, command.handle, *args)
+
+    def test_removes_switch(self):
+        Switch.objects.create(key='test')
+        self.assertIn('test', self.gargoyle)
+
+        call_command('remove_switch', 'test')
+
+        self.assertNotIn('test', self.gargoyle)
+
+    def test_remove_non_switch_doesnt_error(self):
+        self.assertNotIn('idontexist', self.gargoyle)
+
+        call_command('remove_switch', 'idontexist')
+
+        self.assertNotIn('idontexist', self.gargoyle)
