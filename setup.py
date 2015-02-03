4f9beb3eeefcb887e7dba3e115ca888d5253baa7
Matt Robenolt <matt@ydekproductions.com>
2015-02-02 16:53:25 -0800

    https url in setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     version='0.11.0',
     author='DISQUS',
     author_email='opensource@disqus.com',
-    url='http://github.com/disqus/gargoyle',
+    url='https://github.com/disqus/gargoyle',
     description='Gargoyle is a platform built on top of Django which allows you to switch functionality of your application on and off based on conditions.',
     packages=find_packages(exclude=["example_project", "tests"]),
     zip_safe=False,
